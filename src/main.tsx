6ea73fcbef73bfe791ef514f9b23614ab02d0a57
Moun Dahiwale <moundahiwale@yahoo.com>
2026-03-05 12:14:51 +0000

    Add github pages deployment config

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './styles/_reset.scss'; 
 import './index.scss';
 
+// Using HashRouter so Github pages doesn't result in 404s on refresh or direct link access. 
+// For Production, use BrowserRouter and configure the server to handle client side routing properly.
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
